2e406c0be8dfb0ff077675d0d92f88f849738daa
haltablizo <112672692+haltablizo@users.noreply.github.com>
2023-04-28 01:34:47 +0000

    set-up

---
+++ quiz_webpage/quiz.js
@@ -0,0 +1,20 @@
+<!-- the actual game page -->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Quiz</title>
+  <link href="quiz.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <form action='/summary_webpage/summary.html'>
+    <p>:3</p>
+  </form>
+  <script src="quiz.js"></script>
+</body>
+
+</html>
